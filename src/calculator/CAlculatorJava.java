0f8a313df431c8b0918c7a15df08c439da69fea1
msuleman91 <msuleman387@gmail.com>
2019-05-08 14:50:32 -0700

    modify multiply on fb1

--- src/calculator/CAlculatorJava.java
+++ src/calculator/CAlculatorJava.java
@@ -6,10 +6,16 @@ public class CAlculatorJava {
 		
 		return a+b;
 	}
+	
+	public int mul(int a, int b) {
+		
+		return a*b;
+	}
 
 	public static void main(String[] args) {
 		CAlculatorJava cal=new CAlculatorJava();
 		System.out.println(cal.sum(10,10));
+		System.out.println(cal.mul(10,10));
 
 	}
 
